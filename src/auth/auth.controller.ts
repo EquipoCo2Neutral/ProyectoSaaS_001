b2907391414d55afb7fb8f5234f35baea0abcab9
EquipoCo2Neutral <equipodesarrolloc02neutral@gmail.com>
2025-03-14 11:14:40 -0300

    autorizacion con guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from './guard/auth.guard';
+import { request } from 'http';
 
 @Controller('auth')
 export class AuthController {
@@ -23,5 +32,14 @@ export class AuthController {
     loginDto: LoginDto,
   ) {
     return this.authService.login(loginDto);
+  }
+
+  @Get('profile')
+  @UseGuards(AuthGuard)
+  profile(
+    @Request()
+    req,
+  ) {
+    return req.user;
   }
 }
---
+++ src/auth/guard/auth.guard.ts
@@ -0,0 +1,41 @@
+/* En Nest.js, los guards son una funcionalidad que permite controlar si una determinada ruta o endpoint en una aplicación puede ser accedida por un cliente o no.  */
+
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+import { jwtConstants } from '../constants/jwt.constant';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: jwtConstants.secret,
+      });
+      request.user = payload;
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request) {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
