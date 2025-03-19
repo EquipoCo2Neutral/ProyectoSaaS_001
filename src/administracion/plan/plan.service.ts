904372427be0396baa9772cfd07f48e7ec24c58e
Sebas <patodrag13@gmail.com>
2025-03-19 07:43:46 -0300

    update - delete -> Plan

--- src/administracion/plan/plan.service.ts
+++ src/administracion/plan/plan.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePlanDto } from './dto/create-plan.dto';
 import { UpdatePlanDto } from './dto/update-plan.dto';
 import { Plan } from './entities/plan.entity';
@@ -21,16 +21,30 @@ export class PlanService {
     return this.planRepository.find();
   }
 
-  findOne(idPlan: number) {
-    const plan = this.planRepository.findOneBy({ idPlan });
+  async findOne(id: number) {
+    const plan = await this.planRepository.findOne({
+      where : {idPlan:id}
+     });
+
+     if(!plan){
+      let errors : string[] = []
+      errors.push('El plan no existe')
+      throw new NotFoundException(errors);
+
+     }
     return plan;
   }
 
-  update(id: number, updatePlanDto: UpdatePlanDto) {
-    return `This action updates a #${id} plan`;
+  async update(id: number, updatePlanDto: UpdatePlanDto) {
+    const plan = await this.findOne(id)
+    Object.assign(plan, updatePlanDto)
+    
+    return await this.planRepository.save(plan);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} plan`;
+  async remove(id: number) {
+    const plan = await this.findOne(id)
+    await this.planRepository.remove(plan)
+    return "El plan ha sido eliminado";
   }
 }
