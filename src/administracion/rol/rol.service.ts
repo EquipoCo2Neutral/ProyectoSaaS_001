b9ad79f9314c5f5764c9bf0e045cba9a038783c7
Sebas <patodrag13@gmail.com>
2025-03-19 07:47:35 -0300

    update - delete - create(correccion) -> Rol

--- src/administracion/rol/rol.service.ts
+++ src/administracion/rol/rol.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateRolDto } from './dto/create-rol.dto';
 import { UpdateRolDto } from './dto/update-rol.dto';
@@ -12,9 +12,13 @@ export class RolService {
     private readonly rolRepository: Repository<Rol>,
   ) {}
 
-  create(createRolDto: CreateRolDto) {
-    const rol = new Rol();
-    rol.rol = createRolDto.rol;
+  async create(createRolDto: CreateRolDto) {
+    const rol = await this.rolRepository.save(createRolDto)
+    if(!rol){
+      let errors : string[] = []
+      errors.push("No se pudo crear el rol")
+      throw new NotFoundException(errors)
+    }
     return this.rolRepository.save(rol);
   }
 
@@ -22,16 +26,28 @@ export class RolService {
     return this.rolRepository.find();
   }
 
-  findOne(id: number) {
-    const rol = this.rolRepository.findOneBy({ id });
+  async findOne(id: number) {
+    const rol = await this.rolRepository.findOne({
+      where : {id: id}
+    });
+    if(!rol){
+      let errors : string[] = []
+      errors.push("El rol no existe")
+      throw new NotFoundException(errors)
+    }
     return rol;
   }
 
-  update(id: number, updateRolDto: UpdateRolDto) {
-    return `This action updates a #${id} rol`;
+  async update(id: number, updateRolDto: UpdateRolDto) {
+    const rol = await this.findOne(id)
+    Object.assign(rol, updateRolDto)
+
+    return await this.rolRepository.save(rol);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} rol`;
+  async remove(id: number) {
+    const rol = await this.findOne(id)
+    await this.rolRepository.remove(rol)
+    return "El rol ha sido eliminado";
   }
 }
